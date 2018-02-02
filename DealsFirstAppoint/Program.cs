7b377606da338f2431b5e13bedab3c18f727244b
jchavis <jchavis@DBA>
2018-02-02 14:07:56 -0800

    Add project files.

---
+++ DealsFirstAppoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealsFirstAppoint {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
